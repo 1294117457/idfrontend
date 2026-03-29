e438068493fa8c756636a041b870bbc085a5b8a5
unknown <zhouch2@g-bits.com>
2026-03-29 23:20:20 +0800

    总体优化

--- src/router/modules/demand.ts
+++
@@ -1,9 +0,0 @@
-import AccountCreate from '@/views/demand/index.vue'
-import SettingIcon from '@/assets/icons/setting.vue' // 导入 setting.vue 组件
-
-export default {
-  path: 'demand',
-  component: AccountCreate,
-  meta: { title: '条件填写', icon: SettingIcon, sort: 3, desc: '填写分数外的信息' ,coverImg:'@/assets/images/demand_cover.png' }
-
-}
